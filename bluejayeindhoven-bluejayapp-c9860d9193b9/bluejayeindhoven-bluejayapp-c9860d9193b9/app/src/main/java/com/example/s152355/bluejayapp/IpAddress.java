57c74983687e61d6fffc0498767a0d81c269f04f
bramapeters <bramapeters@hotmail.com>
2023-11-06 11:40:12 +0100

    update
    update

--- bluejayeindhoven-bluejayapp-c9860d9193b9/bluejayeindhoven-bluejayapp-c9860d9193b9/app/src/main/java/com/example/s152355/bluejayapp/IpAddress.java
+++
@@ -1,8 +0,0 @@
-package com.example.s152355.bluejayapp;
-
-// Database credentials
-public class IpAddress {
-    public static String IP_ADDRESS = "";
-    public static String DRONE_NAME = "";
-    public static boolean NotificationsEnabled = true;
-}
